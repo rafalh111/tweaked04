fb776a39d579dbf28906f28156c8b73e09d496bd
RafalHachula <rafal.hachula11@gmail.com>
2025-07-07 13:27:46 +0200

    qfasdf

--- turtleStartup.lua
+++ turtleStartup.lua
@@ -3,6 +3,7 @@ local turtleApi = require("turtleProgram03")
 local utils = require("utils")
 local nav = require("nav03")
 local args = { ... }
+local rednet
 
 local data = turtleApi.LoadTurtleState()
 rednet.send(data["baseID"], "new turtle with ID:" .. data.id)
